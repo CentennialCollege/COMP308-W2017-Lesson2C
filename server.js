f322ef9261d7e052b729e18fd99630da5a47652d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-21 16:29:06 -0500

    added connect server with 2 routes

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 // My server will live here - Cthulhu!
+
+// create a reference to the connect module and store a pointer
+// to the connect module in the connect variable
+let connect = require('connect');
+
+// create an instance of the connect server
+let app = connect();
+
+// represents the port address
+const port = 3000;
+
+// listen for requests on a specific port
+app.listen(port);
+console.log(`Server listening at http://localhost/${port}`);
+
+// ROUTING - Mount different routes
+
+// Route for 'hello'
+app.use('/hello', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello, World!");
+    next();
+});
+
+// Default Route for the website '/'
+app.use('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Welcome!");
+    next();
+});
